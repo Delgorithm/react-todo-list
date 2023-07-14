1c20259c642c4eb849490fa202bdcd5dba0e60ed
Delgorithm <delgorithm@gmail.com>
2023-07-14 19:59:27 +0200

    initialization

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TodoForm from './components/TodoForm.js'
+import TodoItem from './components/TodoItem.js'
+import TodoList from './components/TodoList.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <TodoForm />
+    <TodoItem />
+    <TodoList />
   </React.StrictMode>,
 )
