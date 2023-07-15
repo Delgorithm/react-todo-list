f8041912cf1bbef6349af365194fb90da777077d
Delgorithm <delgorithm@gmail.com>
2023-07-15 14:00:51 +0200

    adding one file

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import TodoForm from './components/TodoForm'
 import './index.css'
-import TodoForm from './components/TodoForm.js'
-import TodoItem from './components/TodoItem.js'
-import TodoList from './components/TodoList.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <TodoForm />
-    <TodoItem />
-    <TodoList />
   </React.StrictMode>,
 )
